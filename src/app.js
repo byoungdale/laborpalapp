309084545053ca10d5f4df1f95a4a22ec5f81dc2
byoungdale <brandon.youngdale@gmail.com>
2017-08-03 07:37:20 -0700

    added redux-logger

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger'
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import Router from './Router';
 
 class App extends Component {
   render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk, logger));
 
     return (
       <Provider store={store}>
